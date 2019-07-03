0b1fbf23b23bc663a18a09a128ae424d106dde3c
wojiushizhemeniubi <44362081+wojiushizhemeniubi@users.noreply.github.com>
2019-07-03 14:30:26 +0800

    Add files via upload
    from Xiaokang Wang

--- cachematrix.R
+++ cachematrix.R
@@ -3,8 +3,16 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x = matrix(),...) {
+    m <- NULL
+    set <- function(y){
+        x <<- y
+        m <<- NULL
+    }
+    get <- function() x
+    setM <- function(solve) m <<- solve
+    getM <- function() m
+    list(set = set, get = get, setM = setM, getM = getM)
 }
 
 
@@ -12,4 +20,13 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+    m <- x$getM()
+    if(!is.null(m)){
+        message('TO cached data')
+        return(m)
+    }
+    data <- x$get()
+    m <- solve(data,...)
+    x$setM(m)
+    m
 }
